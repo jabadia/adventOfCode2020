e6b5ee0efe0ed7ba5e1afaf7128a0ee1d2a642fa
Javier Abadía <javi.abadia@gmail.com>
2020-12-01 21:51:45 +0100

    optimization

--- d1/d1p2.js
+++ d1/d1p2.js
@@ -225,6 +225,9 @@ function solve(input) {
     const n = numbers.length;
     for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
+            if ( numbers[i] + numbers[j] >= 2020 ) {
+                continue
+            }
             for (let k = j + 1; k < n; k++) {
                 if (numbers[i] + numbers[j] + numbers[k] === 2020) {
                     return numbers[i] * numbers[j] * numbers[k];
