361b028b4be97807f5b0325b0e2f1bda33a48f26
Javier Abadía <javi.abadia@gmail.com>
2020-12-07 23:18:17 +0100

    add 2nd tests case

--- d7/d7p2.py
+++ d7/d7p2.py
@@ -15,7 +15,16 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.
-""", 126),
+""", 32),
+    TestCase("""
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+""", 126)
 ]
 
 
