3967fc611da2ed7654a99ad8fecb32b56296a57b
Javier Abadía <javi.abadia@gmail.com>
2020-12-16 22:40:51 +0100

    improve test report

--- utils/test_case.py
+++ utils/test_case.py
@@ -3,9 +3,13 @@ class TestCase(object):
         self.case = case
         self.expected = expected
 
+    @staticmethod
+    def _format_case(case):
+        case = repr(case).strip().replace(r'\n', ' ')
+        return case[:97] + ('...' if len(case) > 100 else '')
+
     def check(self, actual):
         if self.expected == actual:
-            case = repr(self.case).strip().replace(r'\n', ' ')
-            print("OK %s %s%s" % (self.expected, case[:100], '...' if len(case) > 100 else ''))
+            print("OK %s %s" % (self.expected, self._format_case(self.case)))
         else:
-            print("FAIL %s, expected %s, got %s" % (self.case, self.expected, actual))
+            print("FAIL expected %s, got %s, %s" % (self.expected, actual, self._format_case(self.case)))
